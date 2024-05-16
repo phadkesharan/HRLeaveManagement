1e3fe225d85f64d782bf82daca49ba089f47e9aa
Sharan Phadke <memariyachirag@gmail.com>
2024-05-16 17:51:29 +0530

    feat: add application services registration file

---
+++ src/Core/HRLeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRLeaveManagement.Application;
+
+public static class ApplicationServicesRegistration
+{
+    public static void ConfigureApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+}
