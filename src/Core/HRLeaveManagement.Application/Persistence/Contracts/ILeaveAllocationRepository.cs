bdcd8825ad3ebaab58117cac766c7140fd81e978
Sharan Phadke <memariyachirag@gmail.com>
2024-05-16 19:00:28 +0530

    fix: add WithDetails method to LeaveAllocation Repository

--- src/Core/HRLeaveManagement.Application/Persistence/Contracts/ILeaveAllocationRepository.cs
+++ src/Core/HRLeaveManagement.Application/Persistence/Contracts/ILeaveAllocationRepository.cs
@@ -4,5 +4,6 @@ namespace HRLeaveManagement.Application.Persistence.Contracts;
 
 public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
 {
-
+    Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id);
+    Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails();
 }
