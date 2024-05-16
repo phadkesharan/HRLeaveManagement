ae88697b4197b660f51ad26f506c1f85828bc239
Sharan Phadke <memariyachirag@gmail.com>
2024-05-16 16:55:30 +0530

    feat: add Domain Objects

---
+++ src/Core/HRLeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace HRLeaveManagement.Domain.Common;
+
+public abstract class BaseDomainEntity
+{
+    public int Id { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public DateTime LastModifiedDate { get; set; }
+
+    public string LastModifiedBy { get; set; } = string.Empty;
+}
---
+++ src/Core/HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using HRLeaveManagement.Domain.Common;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveType : BaseDomainEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public int DefaultDays { get; set; }
+}
