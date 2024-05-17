52b0f4b950d13a94a25eae033450d973c8498853
Sharan Phadke <memariyachirag@gmail.com>
2024-05-17 11:21:10 +0530

    feat: add command for LeaveRequest

---
+++ src/Core/HRLeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Persistence.Contracts;
+using HRLeaveManagement.Domain;
+using MediatR;
+
+namespace HRLeaveManagement.Application;
+
+public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, int>
+{
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    private readonly IMapper _mapper;
+
+    public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<int> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
+    {
+        var leaveRequest = _mapper.Map<LeaveRequest>(request.leaveRequestDto);
+        leaveRequest = await _leaveRequestRepository.Add(leaveRequest);
+        return leaveRequest.Id;
+    }
+}
---
+++ src/Core/HRLeaveManagement.Application/Features/LeaveRequests/Requests/Commands/CreateLeaveRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using HRLeaveManagement.Application.DTOs.LeaveRequest;
+using MediatR;
+
+namespace HRLeaveManagement.Application;
+
+public class CreateLeaveRequestCommand : IRequest<int>
+{
+    public LeaveRequestDto leaveRequestDto { get; set; }
+}
