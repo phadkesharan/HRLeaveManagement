75ea8668a3277ce03cbac77f795bc372cd735056
Sharan Phadke <memariyachirag@gmail.com>
2024-05-20 16:04:19 +0530

    feat: add infrastructure project and setup DbContext

---
+++ src/Infrastructure/HRLeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,40 @@
+﻿using HRLeaveManagement.Domain;
+using HRLeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRLeaveManagement.Persistence;
+
+public class LeaveManagementDbContext: DbContext
+{
+    public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.Now;
+
+            if(entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+
+    }
+
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+}
