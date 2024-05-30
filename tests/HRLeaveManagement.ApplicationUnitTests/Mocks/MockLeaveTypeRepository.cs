9a08be941a3bd9cef7483f0fe03cfe2daeac5dda
Sharan Phadke <phadkesharan@gmail.com>
2024-05-30 10:23:15 +0530

    test: init unit tests project and add Mock repo from leavetypes

---
+++ tests/HRLeaveManagement.ApplicationUnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,64 @@
+﻿using HRLeaveManagement.Application.Persistence.Contracts;
+using HRLeaveManagement.Domain;
+using Moq;
+
+namespace HRLeaveManagement.ApplicationUnitTests;
+
+public static class MockLeaveTypeRepository
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+    {
+        var leaveTypes = new List<LeaveType>
+        {
+            new LeaveType 
+            { 
+                Id = 1,
+                DefaultDays = 1,
+                Name = "Test Casual"
+            },
+
+            new LeaveType
+            {
+                Id = 2,
+                DefaultDays = 1,
+                Name = "Test Paid"
+            }
+        };
+
+        var mockRepo = new Mock<ILeaveTypeRepository>();
+
+        mockRepo.Setup(r => r.GetAll()).ReturnsAsync(leaveTypes.AsReadOnly());
+
+        mockRepo.Setup(r => r.Get(It.IsAny<int>()))
+            .Returns((int id) => leaveTypes.FirstOrDefault(t => t.Id == id));
+
+        mockRepo.Setup(r => r.Exists(It.IsAny<int>()))
+            .Returns((int id) => leaveTypes.Any(t => t.Id == id));
+
+        mockRepo.Setup(r => r.Add(It.IsAny<LeaveType>()))
+            .Returns((LeaveType leaveType) => leaveTypes.Add(leaveType));
+
+        
+        mockRepo.Setup(r => r.Update(It.IsAny<LeaveType>()))
+            .Returns((LeaveType leaveType) => 
+            {
+                var index = leaveTypes.IndexOf(leaveType);
+
+                if(index != -1)
+                {
+                    leaveTypes[index] = leaveType;
+                }
+
+                return Task.CompletedTask;
+            });
+
+        mockRepo.Setup(r => r.Delete(It.IsAny<LeaveType>()))
+            .Returns((LeaveType leaveType) => 
+            {
+                leaveTypes.Remove(leaveType);
+                return Task.CompletedTask;
+            });
+        
+        return mockRepo;
+    }
+}
